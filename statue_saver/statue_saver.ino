bc9bf54c8016a0fa7a064d03d340399d54c72af9
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2019-01-22 17:40:38 +0100

    added rotary encoder support

--- statue_saver/statue_saver.ino
+++ statue_saver/statue_saver.ino
@@ -3,8 +3,21 @@
 
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+ int CLK = 9;  
+ int DT = 8;  
+ int SW = 7; //button
+ int RotPosition = 0; 
+ int rotation;  
+ int value;
+ boolean LeftRight;
+ boolean button = false;
+ 
 int shockSensor = 13;
 Servo arms;
+
+int Pos = 90;
+int Home = 0;
 
 void setup(){
   Wire.begin();
@@ -14,30 +27,82 @@ void setup(){
   Wire.endTransmission(true);
   Serial.begin(9600);
   arms.attach(10);
+  arms.write(Home);
   pinMode (shockSensor, INPUT);
+
+  pinMode (CLK,INPUT);
+   pinMode (DT,INPUT);
+   pinMode(SW,INPUT);
+   rotation = digitalRead(CLK);   
 }
 
 void loop(){
  //getOrientation();
 // printOrientation();
 
-if(digitalRead(shockSensor)==LOW){
+if(digitalRead(SW) == LOW){
+    Serial.println("clicked");
+    if(button==true){
+      button = false;
+      Serial.println("button = false");
+      armsDown();
+    }else{
+      button = true;
+      Serial.println("buton = true");
+      RotPosition = Pos; 
+    }
+    delay(500);
+   }
+
+if(button==false){
+  if(digitalRead(shockSensor)==LOW){
   Serial.println("Shock detected");
   armsUp();
   delay(1000);
   armsDown();
 }
-
-
+}
+if(button==true){
+  value = digitalRead(CLK);
+  arms.write(Pos);
+  if (value != rotation){ // we use the DT pin to find out which way we turning.
+     if (digitalRead(DT) != value) {  // Clockwise
+       RotPosition ++;
+       LeftRight = true;
+     } else { //Counterclockwise
+       LeftRight = false;
+       RotPosition--;
+     }
+     if (LeftRight){ // turning right will turn on red led.
+       Serial.println ("counterclockwise");
+     }else{        // turning left will turn on green led.   
+       Serial.println("clockwise");
+     }
+     Serial.print("Encoder RotPosition: ");
+     Serial.println(RotPosition);
+     if(Pos<92 && Pos>Home){
+      Pos = RotPosition;
+     
+     }
+     if(RotPosition>90){
+      RotPosition = 89;
+     }
+     if(RotPosition<Home+2){
+      RotPosition = 4;
+     }
+}
+rotation = value;
+} 
+   
  
 }
 
 void armsUp(){
-  arms.write(90);
+  arms.write(Pos);
   Serial.println("arms up");
 }
 void armsDown(){
-  arms.write(0);
+  arms.write(Home);
   Serial.println("arms down");
 }
 void getOrientation(){
